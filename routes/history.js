78105315a11a9c07c80a87891561d7dbd569301c
Vincent R <131643744+vincentr23@users.noreply.github.com>
2025-05-14 14:30:14 -0700

    Update history.js

--- routes/history.js
+++ routes/history.js
@@ -15,32 +15,36 @@ router.get('/', async (req, res) => {
   try {
     if (type === 'selections') {
       // Fetch all selections from the database
-      const selections = await db.find('SearchHistorySelection');
+      const cursor = await db.find('SearchHistorySelection');
       
       // Convert the cursor to an array
-      const cursor = await selections.toArray();
+      const selections = await cursor.toArray();
 
       // Check if the cursor is empty
-      if (cursor.length === 0) {
+      if (selections.length === 0) {
         return res.status(204).end(); // No content
       }
+      const selectionsList = selections.map(doc => ({ 
+          title: doc.game.info.title,
+          dealPrice: doc.game.deals[0].price
+        }));
 
-      return res.json(cursor);
+      return res.json(selectionsList);
     }
     if (type === 'keywords') {
       // Fetch all keywords from the database
-      const keywords = await db.find('SearchHistoryKeyword');
+      const cursor = await db.find('SearchHistoryKeyword');
 
       // Convert the cursor to an array
-      const cursor = await keywords.toArray();
+      const keywords = await cursor.toArray();
 
       // Check if the cursor is empty
-      if (cursor.length === 0) {
+      if (keywords.length === 0) {
         return res.status(204).end(); // No content
       }
 
       // Only return the keyword field from each document
-      const keywordList = cursor.map(doc => ({ keyword: doc.keyword }));
+      const keywordList = keywords.map(doc => ({ keyword: doc.keyword }));
       return res.json(keywordList);
     }
 
